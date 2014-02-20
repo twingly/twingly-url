0e0137e0a0d78631f2de532718e8e8bf7890a6fb
Johan Eckerström <johan@duh.se>
2014-02-20 19:44:35 +0100

    Fix performance test, should be testing normalize_url

--- test/profile/normalize_performance_test.rb
+++ test/profile/normalize_performance_test.rb
@@ -4,7 +4,7 @@ require 'test_profile'
 class NormalizerPerformanceTest < Test::Unit::TestCase
   context ".normalize_url" do
     measure "normalizing a short URL", 1000 do
-      Twingly::URL::Normalizer.normalize('http://www.duh.se/')
+      Twingly::URL::Normalizer.normalize_url('http://www.duh.se/')
     end
   end
 end
