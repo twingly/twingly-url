cdbab00e705a6c689349b905bac7e9afee18fd52
Johan Eckerström <johan@duh.se>
2014-02-20 14:54:30 +0100

    Refactor .normalize

--- lib/twingly-url-normalizer.rb
+++ lib/twingly-url-normalizer.rb
@@ -8,15 +8,23 @@ require 'uri'
 module Twingly
   module URL
     class Normalizer
-      def self.normalize(potential_url)
-        PostRank::URI.extract(potential_url).map do |url|
-          subdomain = Domainatrix.parse(url).subdomain
-          uri = URI.parse(url)
-          if subdomain.empty?
-            uri.host = "www.#{uri.host}"
-          end
-          uri.to_s
+      def self.normalize(potential_urls)
+        extract_urls(potential_urls).map do |url|
+          normalize_url(url)
         end
+      end
+
+      def self.extract_urls(potential_urls)
+        PostRank::URI.extract(potential_urls)
+      end
+
+      def self.normalize_url(url)
+        subdomain = Domainatrix.parse(url).subdomain
+        uri = URI.parse(url)
+        if subdomain.empty?
+          uri.host = "www.#{uri.host}"
+        end
+        uri.to_s
       end
     end
   end
--- test/unit/normalization_test.rb
+++ test/unit/normalization_test.rb
@@ -1,29 +1,70 @@
 require 'test_helper'
 
 class NormalizerTest < Test::Unit::TestCase
-  context "normalize" do
+  context ".normalize" do
+    setup do
+      @normalizer = Twingly::URL::Normalizer
+    end
+
+    should "accept a String" do
+      assert @normalizer.normalize("")
+    end
+
+    should "accept an Array" do
+      assert @normalizer.normalize([])
+    end
+  end
+
+  context ".extract_urls" do
     setup do
       @normalizer = Twingly::URL::Normalizer
     end
 
     should "detect two urls in a String" do
       urls = "http://blog.twingly.com/ http://twingly.com/"
-      response = @normalizer.normalize(urls)
+      response = @normalizer.extract_urls(urls)
 
       response.size.must_equal 2
     end
 
     should "detect two urls in an Array" do
       urls = %w(http://blog.twingly.com/ http://twingly.com/)
-      response = @normalizer.normalize(urls)
+      response = @normalizer.extract_urls(urls)
 
       response.size.must_equal 2
     end
 
     should "return an Array" do
-      response = @normalizer.normalize(nil)
+      response = @normalizer.extract_urls(nil)
 
       response.must_be_instance_of Array
+    end
+  end
+
+  context ".normalize_url" do
+    setup do
+      @normalizer = Twingly::URL::Normalizer
+    end
+
+    should "add www if host is missing a subdomain" do
+      url = "http://twingly.com/"
+      result = @normalizer.normalize_url(url)
+
+      assert_equal "http://www.twingly.com/", result
+    end
+
+    should "not add www if the host has a subdomain" do
+      url = "http://blog.twingly.com/"
+      result = @normalizer.normalize_url(url)
+
+      assert_equal "http://blog.twingly.com/", result
+    end
+
+    should "keep www if the host already has it" do
+      url = "http://www.twingly.com/"
+      result = @normalizer.normalize_url(url)
+
+      assert_equal "http://www.twingly.com/", result
     end
   end
 end
