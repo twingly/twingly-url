f840cda4dcb5ca7cea0bb055f58847b6eec62d01
Johan Eckerström <johan@duh.se>
2014-07-15 14:15:48 +0200

    Use module function

--- lib/twingly/url/normalizer.rb
+++ lib/twingly/url/normalizer.rb
@@ -5,18 +5,20 @@ PublicSuffix::List.private_domains = false
 
 module Twingly
   module URL
-    class Normalizer
-      def self.normalize(potential_urls)
+    module Normalizer
+      module_function
+
+      def normalize(potential_urls)
         extract_urls(potential_urls).map do |potential_url|
           normalize_url(potential_url)
         end.compact
       end
 
-      def self.extract_urls(potential_urls)
+      def extract_urls(potential_urls)
         Array(potential_urls).map(&:split).flatten
       end
 
-      def self.normalize_url(potential_url)
+      def normalize_url(potential_url)
         uri    = Addressable::URI.heuristic_parse(potential_url)
         domain = PublicSuffix.parse(uri.host)
 
