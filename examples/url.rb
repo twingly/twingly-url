5b615d15d3a22b75adba7c8da312edfcaacf61c7
Johan Eckerström <johan@duh.se>
2016-09-15 13:36:27 +0200

    DRY up to urls example
    This will print userinfo, user and password for all URLs, which it
    didn't do before.

--- examples/url.rb
+++ examples/url.rb
@@ -1,79 +1,40 @@
 require "bundler/setup"
 require_relative "../lib/twingly/url"
 
-url_as_string = "http://www.twingly.co.uk/search"
-url = Twingly::URL.parse(url_as_string)
+def print_url_details(url_as_string)
+  url = Twingly::URL.parse(url_as_string)
+
+  puts "url = Twingly::URL.parse(\"#{url_as_string}\")"
+  puts "url.scheme              # => \"#{url.scheme}\""
+  puts "url.trd                 # => \"#{url.trd}\""
+  puts "url.sld                 # => \"#{url.sld}\""
+  puts "url.tld                 # => \"#{url.tld}\""
+  puts "url.ttld                # => \"#{url.ttld}\""
+  puts "url.domain              # => \"#{url.domain}\""
+  puts "url.host                # => \"#{url.host}\""
+  puts "url.origin              # => \"#{url.origin}\""
+  puts "url.path                # => \"#{url.path}\""
+  puts "url.without_scheme      # => \"#{url.without_scheme}\""
+  puts "url.userinfo            # => \"#{url.userinfo}\""
+  puts "url.user                # => \"#{url.user}\""
+  puts "url.password            # => \"#{url.password}\""
+  puts "url.valid?              # => \"#{url.valid?}\""
+end
 
 puts "require \"twingly/url\""
 
 puts
 
-puts "url = Twingly::URL.parse(\"#{url_as_string}\")"
-puts "url.scheme              # => \"#{url.scheme}\""
-puts "url.trd                 # => \"#{url.trd}\""
-puts "url.sld                 # => \"#{url.sld}\""
-puts "url.tld                 # => \"#{url.tld}\""
-puts "url.ttld                # => \"#{url.ttld}\""
-puts "url.domain              # => \"#{url.domain}\""
-puts "url.host                # => \"#{url.host}\""
-puts "url.origin              # => \"#{url.origin}\""
-puts "url.path                # => \"#{url.path}\""
-puts "url.without_scheme      # => \"#{url.without_scheme}\""
-puts "url.valid?              # => \"#{url.valid?}\""
+print_url_details("http://www.twingly.co.uk/search")
 
 puts
 
-unicode_idn_url_as_string = "http://räksmörgås.макдональдс.рф/foo"
-url = Twingly::URL.parse(unicode_idn_url_as_string)
-
-puts "url = Twingly::URL.parse(\"#{unicode_idn_url_as_string}\")"
-puts "url.scheme              # => \"#{url.scheme}\""
-puts "url.trd                 # => \"#{url.trd}\""
-puts "url.sld                 # => \"#{url.sld}\""
-puts "url.tld                 # => \"#{url.tld}\""
-puts "url.ttld                # => \"#{url.ttld}\""
-puts "url.domain              # => \"#{url.domain}\""
-puts "url.host                # => \"#{url.host}\""
-puts "url.origin              # => \"#{url.origin}\""
-puts "url.path                # => \"#{url.path}\""
-puts "url.without_scheme      # => \"#{url.without_scheme}\""
-puts "url.valid?              # => \"#{url.valid?}\""
+print_url_details("http://räksmörgås.макдональдс.рф/foo")
 
 puts
 
-ascii_idn_url_as_string = "http://xn--rksmrgs-5wao1o.xn--80aalb1aicli8a5i.xn--p1ai/foo"
-url = Twingly::URL.parse(ascii_idn_url_as_string)
-
-puts "url = Twingly::URL.parse(\"#{ascii_idn_url_as_string}\")"
-puts "url.scheme              # => \"#{url.scheme}\""
-puts "url.trd                 # => \"#{url.trd}\""
-puts "url.sld                 # => \"#{url.sld}\""
-puts "url.tld                 # => \"#{url.tld}\""
-puts "url.ttld                # => \"#{url.ttld}\""
-puts "url.domain              # => \"#{url.domain}\""
-puts "url.host                # => \"#{url.host}\""
-puts "url.origin              # => \"#{url.origin}\""
-puts "url.path                # => \"#{url.path}\""
-puts "url.without_scheme      # => \"#{url.without_scheme}\""
-puts "url.valid?              # => \"#{url.valid?}\""
+print_url_details("http://xn--rksmrgs-5wao1o.xn--80aalb1aicli8a5i.xn--p1ai/foo")
 
 puts
 
-url_as_string = "https://admin:correcthorsebatterystaple@example.com/"
-url = Twingly::URL.parse(url_as_string)
-
-puts "url = Twingly::URL.parse(\"#{url_as_string}\")"
-puts "url.scheme              # => \"#{url.scheme}\""
-puts "url.trd                 # => \"#{url.trd}\""
-puts "url.sld                 # => \"#{url.sld}\""
-puts "url.tld                 # => \"#{url.tld}\""
-puts "url.ttld                # => \"#{url.ttld}\""
-puts "url.domain              # => \"#{url.domain}\""
-puts "url.host                # => \"#{url.host}\""
-puts "url.origin              # => \"#{url.origin}\""
-puts "url.path                # => \"#{url.path}\""
-puts "url.without_scheme      # => \"#{url.without_scheme}\""
-puts "url.userinfo            # => \"#{url.userinfo}\""
-puts "url.user                # => \"#{url.user}\""
-puts "url.password            # => \"#{url.password}\""
-puts "url.valid?              # => \"#{url.valid?}\""
+print_url_details("https://admin:correcthorsebatterystaple@example.com/")
